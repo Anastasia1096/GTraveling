f3e7977ce0f292811eefd4ddc9f795dc1299b6f8
Anastasia <anastasia.gorrino@gmail.com>
2019-06-08 21:22:35 +0200

    add map and database

--- platforms/android/app/src/main/assets/www/js/new-itinerary.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('input[type="radio"]').click(function(){
-    	var demovalue = $(this).val(); 
-        $("div.city").hide();
-        $("#show"+demovalue).show();
-    });
-});
